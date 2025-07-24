1f5c7b38f767c6f2ecb5e070d143a2e76531f619
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-07-24 22:47:40 +0200

    initialize Spring Boot project with Maven  and essential configurations.

---
+++ src/main/java/loboda/nicolae/U5_W6_D4_SpringWeb/U5W6D4SpringWebApplication.java
@@ -0,0 +1,13 @@
+package loboda.nicolae.U5_W6_D4_SpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W6D4SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W6D4SpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loboda/nicolae/U5_W6_D4_SpringWeb/U5W6D4SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package loboda.nicolae.U5_W6_D4_SpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W6D4SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
